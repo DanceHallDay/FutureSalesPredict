26d791c815bae67753015fd43400cc321762fa89
levermory <lev.taborov@icloud.com>
2022-11-09 16:32:48 +0300

    Changes to preprocessing and training processes.

--- dags/train.py
+++ dags/train.py
@@ -1,22 +1,21 @@
-from .preprocessing import IDataset
-from .preprocessing.SlidingWIndowCV import SlidingWindowSplitCV
 from sklearn.metrics import (
     mean_absolute_error,
     mean_squared_error
 )
 import numpy as np
 import pandas as pd
-# import lightgbm as lgb
+from dags.preprocessing.SlidingWIndowCV import SlidingWindowSplitCV
+from dags.preprocessing.Dataset import Dataset
 from catboost import CatBoostRegressor, Pool
 import gc
 
 
-def train(train_data: IDataset, test_data: IDataset, cv: SlidingWindowSplitCV,):
+def train(train_data: Dataset, cv: SlidingWindowSplitCV,):
 
     models = []
     X_train = train_data.get_data()
     y_train = train_data.get_labels()
-    X_test = test_data.get_data()
+    # X_test = test_data.get_data()
 
     mse_result = []
     mae_result = []
@@ -28,51 +27,57 @@ def train(train_data: IDataset, test_data: IDataset, cv: SlidingWindowSplitCV,):
     for train_idx, valid_idx in cv.split(X_train, 'date_block_num'):
         curr_X_train = X_train.loc[train_idx]
         curr_y_train = y_train.loc[train_idx]
+
         curr_X_valid = X_train.loc[valid_idx]
         curr_y_valid = y_train.loc[valid_idx]
 
         train_dataset = Pool(
             curr_X_train,
             curr_y_train,
-            cat_feature=['date_block_num', 'item_id', 'shop_id',
-                         'shop_city', 'shop_type', 'item_category', 'item_subcategory'],
-            # text_features=['shop_name', 'item_name', 'item_category_name']
+            cat_features=['date_block_num', 'item_id', 'shop_id',
+                         'shop_city', 'shop_type', 'item_category', 'item_subcategory', 'item_fixed_category'],
+            text_features=['shop_name', 'item_name', 'item_category_name']
         )
 
         valid_dataset = Pool(
             curr_X_valid,
             curr_y_valid,
-            cat_feature=['date_block_num', 'item_id', 'shop_id',
-                         'shop_city', 'shop_type', 'item_category', 'item_subcategory'],
-            # text_features=['shop_name', 'item_name', 'item_category_name']
+            cat_features=['date_block_num', 'item_id', 'shop_id',
+                         'shop_city', 'shop_type', 'item_category', 'item_subcategory', 'item_fixed_category'],
+            text_features=['shop_name', 'item_name', 'item_category_name']
         )
 
 
-        params = {
-            'depth': 10,
-            'loss_function': 'RMSE',
-            'num_leaves': 31,
-            'learning_rate': 0.05,
-            'verbose': None
-        }
+        param = {
+            # 'iterations': 100,
+            # 'depth': 3,
+            # 'loss_function': 'RMSE',
+            # 'learning_rate': 0.5,
+            # 'verbose': True,
+            # 'early_stopping_rounds': 5,
+            # 'task_type': 'CPU'
+            }
 
-        curr_model = CatBoostRegressor(params)
+
+        curr_model = CatBoostRegressor(early_stopping_rounds=5,
+                                       depth=5,
+                                       learning_rate=0.5)
         curr_model.fit(train_dataset, eval_set=valid_dataset)
 
         models.append(curr_model)
 
-        train_predict = curr_model.predict(curr_X_train)
-        test_predict.append(curr_model.predict(X_test))
+        # train_predict = curr_model.predict(curr_X_train)
+        # # test_predict.append(curr_model.predict(X_test))
 
-        mse = mean_squared_error(y_train, train_predict)
-        mae = mean_absolute_error(y_train, train_predict)
+        # mse = mean_squared_error(y_train, train_predict)
+        # mae = mean_absolute_error(y_train, train_predict)
 
 
-        mse_result.append(mse)
-        mae_result.append(mae)
-        feature_importances.append(curr_model.get_feature_importance())
+        # mse_result.append(mse)
+        # mae_result.append(mae)
+        # feature_importances.append(curr_model.get_feature_importance())
 
-        del curr_X_train, curr_y_train, curr_X_valid, curr_y_valid
-        gc.collect()
+        # del curr_X_train, curr_y_train, curr_X_valid, curr_y_valid
+        # gc.collect()
 
-    return models, test_predict
+    return {'models': models, 'test_predict': test_predict, 'mse': mse_result, 'mae': mae_result}
