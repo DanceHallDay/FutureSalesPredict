f7d50fb3953195e7a9205c70e07be2ab740b6d1a
levermory <lev.taborov@icloud.com>
2022-11-09 18:10:39 +0300

    Added setup and init files to create python package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='futuresalespackage',
+    version='0.0.1',
+    author='levermory',
+    author_email='lev.taborov@icloud.com',
+    packages=['dags', 'dags.preprocessing'],
+    description='An awesome package that does something'
+)
